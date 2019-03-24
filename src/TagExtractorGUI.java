9f795913caad453ab97774b9d091f0bb81ad28db
Revelol <thakurj2000@gmail.com>
2019-03-24 18:10:29 -0400

    added .gitignore and README
    added the two text files
    
    created GUI file
    
    Created Runner
    
    Designed the GUI
    
    added a extractTags method

--- src/TagExtractorGUI.java
+++ src/TagExtractorGUI.java
@@ -2,6 +2,11 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.*;
+
+
 import java.io.File;
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileSystemView;
@@ -9,14 +14,15 @@ import javax.swing.filechooser.FileSystemView;
 public class TagExtractorGUI extends JFrame {
     JPanel main, top, middle, bottom;
     JLabel label, textFileLabel, stopFileLabel;
-    JButton tagButton, stopButton, quitBtn, extractButton;
-    JTextArea fortune;
+    JButton tagButton, stopButton, quitBtn, extractButton, saveButton;
+    JTextArea tags;
     JScrollPane scrollPane;
     File tagFile, stopFile;
+    StringBuffer sb;
 
     public TagExtractorGUI()
     {
-        super("Tag Extractorr");
+        super("Tag Extractor");
         // configure the GUI
         main = new JPanel();
 
@@ -80,8 +86,8 @@ public class TagExtractorGUI extends JFrame {
     }
     private void createMiddlePanel()
     {
-        this.fortune = new JTextArea();
-        this.scrollPane = new JScrollPane(this.fortune);
+        tags = new JTextArea();
+        scrollPane = new JScrollPane(tags);
     }
     private void createBottomPanel()
     {
@@ -123,9 +129,26 @@ public class TagExtractorGUI extends JFrame {
         extractButton = new JButton("Extract Tag");
 
         extractButton.addActionListener((ActionEvent ae) -> {
+           if (tagFile != null && stopFile != null) {
+               extractTags();
+           } else {
+               JOptionPane.showMessageDialog(null, "You must select a text and stop file");
+           }
+        });
+        bottom.add(extractButton);
+
+        saveButton = new JButton("Save File");
+        saveButton.addActionListener((ActionEvent ae ) -> {
+          if (sb != null ){
+              tagsWriter();
+              JOptionPane.showMessageDialog(null, "You have successfully saved the file");
+          } else {
+              JOptionPane.showMessageDialog(null, "No tags available to save!");
+          }
 
         });
-        bottom.add(stopButton);
+        bottom.add(saveButton);
+
 
         quitBtn = new JButton("Quit");
         quitBtn.addActionListener((ActionEvent ae) -> {
@@ -136,6 +159,92 @@ public class TagExtractorGUI extends JFrame {
         bottom.add(quitBtn);
     }
 
+    private void extractTags() {
 
+        Set<String> stopWords = new TreeSet<>();
+        try {
+            Scanner stopFileReader = new Scanner(stopFile);
+            while (stopFileReader.hasNext()) {
+                String word = clean(stopFileReader.next());
+                stopWords.add(word);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("stop file not found:"+e);
+        }
+
+        Map<String, Integer> frequencies = new TreeMap<>();
+        try {
+            Scanner in = new Scanner(tagFile);
+
+            while (in.hasNext()) {
+                String word = clean(in.next());
+
+                if (!stopWords.contains(word)) {
+
+                    // Get the old frequency count
+
+                    Integer count = frequencies.get(word);
+
+                    // If there was none, put 1; otherwise, increment the count
+                    if (count == null) {
+                        count = 1;
+                    } else {
+                        count = count + 1;
+                    }
+                    frequencies.put(word, count);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("text file not found:"+e);
+        }
+
+        // Print all words and counts
+        sb = new StringBuffer();
+        for (String key : frequencies.keySet())
+        {
+            sb.append(key +":" +frequencies.get(key) + "\n");
+        }
+        tags.setText(sb.toString());
+
+    }
+
+
+    /**
+     Removes characters from a string that are not letters.
+     @param s a string
+     @return a string with all the letters from s
+     */
+    public String clean(String s)
+    {
+        String r = "";
+        for (int i = 0; i < s.length(); i++)
+        {
+            char c = s.charAt(i);
+            if (Character.isLetter(c))
+            {
+                r = r + c;
+            }
+        }
+        return r.toLowerCase();
+    }
+
+
+    public void tagsWriter()
+    {
+        String fileName = "tags.txt";
+        try{
+            PrintWriter file = new PrintWriter(fileName);
+            file.print(sb.toString());
+            file.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("error: file not found: ");
+            e.printStackTrace();
+
+        } catch (Exception e) {
+            System.out.println("error: exception not found: ");
+            e.printStackTrace();
+
+        }
+    }
 
 }
